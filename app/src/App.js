5a575e3b632141b91733b60cadf8517ad04e68c0
derique96 <fowlkes.derique@gmail.com>
2023-08-30 16:58:30 -0400

    added react router dom dependency

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Resume from "./pages/Resume";
 
 function App() {
   return (
@@ -18,9 +16,7 @@ function App() {
               <Route path="/React-Portfolio" element={<About />} />
               <Route path="/" element={<About />} />
               <Route path="/about" element={<About />} />
-              <Route path="/portfolio" element={<Portfolio />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/resume" element={<Resume />} />
             </Routes>
           </div>
         </Router>
