903335fed5aa385f57f2b1d82b66c80e15ec8bf6
derique96 <fowlkes.derique@gmail.com>
2023-09-11 21:21:17 -0400

    social media done

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Navbar from './components/Navbar';
 
@@ -15,8 +14,6 @@ function App() {
             <Header />
             <Navbar />
             <Routes>
-              <Route path="/" element={<About />} />
-              <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
           </div>
--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import facebook from '../images/fb logo.png'
+import twitter from '../images/twitter logo.png'
+import instagram from '../images/instagram logo.png'
+import gmail from '../images/gmail logo.png'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div class="social-media"
+      style={{
+        display: 'flex',
+        justifyContent: 'center'
+
+      }}
+    >
+            <img
+              src={facebook} 
+              alt = "facebook logo"
+              style = {{
+                width: '25px',
+                height: '25%',
+              }} 
+            />
+            <img 
+              src= {twitter} 
+              alt= "twitter logo"
+              style = {{
+                width: '25px',
+                height: '25%',
+              }} 
+            />
+            <img 
+              src = {instagram}
+              alt ="instagram logo"
+              style = {{
+                width: '24px',
+                height: '24px',
+              }} 
+            />
+            <img 
+            src= {gmail}
+            alt="gmail logo"
+            style = {{
+              width: '49px',
+              height: '25%',
+            }} 
+          />
+    </div>
   )
 }
